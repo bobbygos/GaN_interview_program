c06fd1440a0ae3671c9d6feddbc625d5ce0569d3
bobbygos <bobby.gosselin@yahoo.com>
2019-04-29 20:32:41 -0500

    Added tests for multiple of five and contains 5

--- interview_program/fizzBuzzPlus_test.py
+++ interview_program/fizzBuzzPlus_test.py
@@ -21,3 +21,20 @@ def test_thirteen_contains_three(setup):
 def test_fourteen_does_not_contain_three(setup):
     test = setup
     assert test.containsThree(14) == False
+
+def test_five_is_mutiple_of_five(setup):
+    test = setup
+    assert test.multipleOfFive(5)
+    
+def test_four_is_not_multiple_of_five(setup):
+    test = setup
+    assert test.multipleOfFive(4) == False
+
+def test_fifteen_contains_five(setup):
+    test = setup
+    assert test.containsFive(15)
+    
+def test_fourteen_does_not_contain_five(setup):
+    test = setup
+    assert test.containsFive(14) == False
+    
