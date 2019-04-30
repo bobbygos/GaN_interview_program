90bca2c91cee931f58a9205da105e46881b16373
bobbygos <bobby.gosselin@yahoo.com>
2019-04-29 20:29:03 -0500

    Added fixture for test setup

--- interview_program/fizzBuzzPlus_test.py
+++ interview_program/fizzBuzzPlus_test.py
@@ -1,17 +1,23 @@
 import fizzBuzzPlus
+import pytest
 
-def test_three_is_mutiple_of_three():
+@pytest.fixture(scope='session')
+def setup():
     test = fizzBuzzPlus.FizzBuzzPlus()
+    yield test
+    
+def test_three_is_mutiple_of_three(setup):
+    test = setup
     assert test.multipleOfThree(3)
     
-def test_four_is_not_multiple_of_three():
-    test = fizzBuzzPlus.FizzBuzzPlus()
+def test_four_is_not_multiple_of_three(setup):
+    test = setup
     assert test.multipleOfThree(4) == False
 
-def test_thirteen_contains_three():
-    test = fizzBuzzPlus.FizzBuzzPlus()
+def test_thirteen_contains_three(setup):
+    test = setup
     assert test.containsThree(13)
-
-def test_fourteen_does_not_contain_three():
-    test = fizzBuzzPlus.FizzBuzzPlus()
+    
+def test_fourteen_does_not_contain_three(setup):
+    test = setup
     assert test.containsThree(14) == False
