6ba12637d6305a6438c084552c12aa1e6ac34546
bobbygos <bobby.gosselin@yahoo.com>
2019-04-29 20:13:10 -0500

    Renamed a couple test names; added testcases for contains 3

--- interview_program/fizzBuzzPlus_test.py
+++ interview_program/fizzBuzzPlus_test.py
@@ -1,10 +1,17 @@
 import fizzBuzzPlus
 
-def test_mutiple_of_three_with_three():
+def test_three_is_mutiple_of_three():
     test = fizzBuzzPlus.FizzBuzzPlus()
     assert test.multipleOfThree(3)
     
-def test_multiple_of_three_with_four():
+def test_four_is_not_multiple_of_three():
     test = fizzBuzzPlus.FizzBuzzPlus()
     assert test.multipleOfThree(4) == False
 
+def test_thirteen_contains_three():
+    test = fizzBuzzPlus.FizzBuzzPlus()
+    assert test.containsThree(13)
+
+def test_fourteen_does_not_contain_three():
+    test = fizzBuzzPlus.FizzBuzzPlus()
+    assert test.containsThree(14)
